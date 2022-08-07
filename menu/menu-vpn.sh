6d7c5822f18f90279993e0c2ab78719804c185ec
fromhell26 <109668151+fromhell26@users.noreply.github.com>
2022-08-07 17:59:06 +0700

    Update menu-vpn.sh

--- menu/menu-vpn.sh
+++ menu/menu-vpn.sh
@@ -10,7 +10,7 @@ color3='\e[0m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
 clear
-cat /usr/bin/bannerVPN | lolcat
+toilet --gay -f slant -t " ENVY PROJECT"
 echo -e""
 echo -e "${color1}1${color3}.$bd  Secure Socket Tunneling Protocol (${color2}menu-sstp${color3})"
 echo -e "${color1}2${color3}.$bd  Layer Two Tunneling Protocol (${color2}menu-l2tp${color3})"
