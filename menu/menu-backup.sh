bf17536d157f6109a20e7c1db74a5facf020946f
fromhell26 <109668151+fromhell26@users.noreply.github.com>
2022-08-07 17:52:24 +0700

    Update menu-backup.sh

--- menu/menu-backup.sh
+++ menu/menu-backup.sh
@@ -10,7 +10,7 @@ color3='\e[0m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
 clear
-cat /usr/bin/bannerBACKUP | lolcat
+toilet --gay -f slant -t " ENVY PROJECT"
 echo -e""
 echo -e "${color1}1${color3}.$bd Autobackup Data VPS (${color2}autobackup${color3})"
 echo -e "${color1}2${color3}.$bd Backup Manual Data VPS (${color2}backup${color3})"
